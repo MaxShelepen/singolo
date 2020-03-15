d4eeb4e7850c531bc3f4678a9685e2f7e481dd1a
maxshelepen <maxshelepen25@gmail.com>
2020-03-15 12:04:50 +0300

    feat: add script.js

---
+++ script.js
@@ -0,0 +1,137 @@
+const menu = document.querySelector('#menu');
+
+
+menu.addEventListener('click', (event) => {
+    menu.querySelectorAll('.navbar__list-item > a').forEach(el => el.classList.remove('active'));
+    event.target.classList.add('active');
+});
+
+
+const phoneScreen = document.querySelector('.phone__screen__slide-2');
+const phone1__screenSlide1 = document.querySelector('.phone1__screen-Slide1');
+const phone2__screenSlide1 = document.querySelector('.phone2__screen-Slide1');
+const slider = document.querySelector('.slider');
+const slide1 = document.querySelector('.slider__slide-1');
+const slide2 = document.querySelector('.slider__slide-2');
+
+slider.addEventListener('click', (event) => {
+
+    if (event.target.className.includes('arrow')) {
+        if (slide1.className.includes('active')) {
+            slide1.classList.remove('active');
+            phone1__screenSlide1.classList.add('blockRemove');
+            phone2__screenSlide1.classList.add('blockRemove');
+            phoneScreen.querySelector('.screen').classList.remove('blockRemove');
+            slide2.classList.add('active');
+            slider.classList.add('background__Slide-2');
+        } else {
+            slide1.classList.add('active');
+            phoneScreen.querySelector('.screen').classList.add('blockRemove');
+            phone1__screenSlide1.classList.remove('blockRemove');
+            phone2__screenSlide1.classList.remove('blockRemove');
+            slide2.classList.remove('active');
+            slider.classList.remove('background__Slide-2');
+            slider.classList.add('background__Slide-1');
+        }
+        
+
+    }
+
+    
+});
+
+
+
+phoneScreen.addEventListener('click', (event) => {
+    const className = event.target.className;
+        if (className.includes('off')) {
+            phoneScreen.querySelector('.screen').classList.remove('off');
+    
+        } else {
+            phoneScreen.querySelector('.screen').classList.add('off');
+    
+        };
+    });
+
+
+    phone1__screenSlide1.addEventListener('click', (event) => {
+        const className = event.target.className;
+            if (className.includes('off')) {
+                phone1__screenSlide1.querySelector('.screen').classList.remove('off');
+        
+            } else {
+                phone1__screenSlide1.querySelector('.screen').classList.add('off');
+        
+            };
+        });
+
+        phone2__screenSlide1.addEventListener('click', (event) => {
+            const className = event.target.className;
+                if (className.includes('off')) {
+                    phone2__screenSlide1.querySelector('.screen').classList.remove('off');
+            
+                } else {
+                    phone2__screenSlide1.querySelector('.screen').classList.add('off');
+            
+                };
+            });
+
+            const portfolio = document.querySelector('.portfolio');
+
+
+
+            portfolio.addEventListener('click', (event) => {
+                const portfolioImages =  document.querySelector('.portfolio__3column');
+                const images = portfolioImages.querySelectorAll('.portfolio__item');
+
+                if(event.target.className === 'portfolio__tag') {
+                    const portfolioTags = document.querySelector('.portfolio__tags');
+               
+                    portfolioTags.querySelectorAll('.portfolio__tag').forEach(el => el.classList.remove('active__tag'));
+                    event.target.classList.add('active__tag');
+                    
+                    const newImages = [...images].sort((a,b) => Math.random() - 0.5);
+                   portfolioImages.append(...newImages);
+                   
+                }
+          if(event.target.className === 'images__item') {
+            images.forEach(el => el.querySelector('.images__item').classList.remove('active__images'));
+                    event.target.classList.add('active__images');
+
+          }
+            });
+
+const form = document.querySelector('.form');
+const button = document.querySelector('.btn');
+document.body.addEventListener('submit' , (e) =>{
+    e.preventDefault();
+});
+  button.addEventListener('click', (event) => {
+      const formResponse = `<div id="form-response-container">
+      <form  action="/" method="POST" id="form-response-form">
+        <p class="response-message"><h3 class = "pesponse-title">Письмо отправлено</h3></p>
+        <p class = "topic">${form.subject.value !== '' ? '<h4>Тема</h4>' + ' ' + form.subject.value  : 'Без темы' }</p>
+        <p class = "description">${form.blog.value !== '' ? '<h4>Описание</h4>' + '' + form.blog.value : 'Без описания' }</p>
+        <input type="submit" class = "form__response-button"  value="Ok">
+        
+      </form>
+    </div>`; 
+      
+showModal(formResponse);
+  });
+
+function showModal(text) {
+   const modalWindow = document.createElement('div');
+   modalWindow.classList.add('modal-window');
+   const quoteInner = document.querySelector('.quote__inner');
+   modalWindow.innerHTML = text;
+   quoteInner.append(modalWindow);
+   document.querySelector('.form__response-button').addEventListener('click',removeModal);
+};
+
+function removeModal() {
+    document.querySelector('.modal-window').remove();
+};
+ 
+
+            
